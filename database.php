41f630d4a1936ef147e904c3e5c6f884ebe2e546
buvaneish <sbuvaneish@gmail.com>
2019-10-25 17:56:35 -0500

    Heroku Changes

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = 'localhost:3306';
-$username = 'root';
-$password = 'password';
-$database = 'event_management';
+$server = 'us-cdbr-iron-east-05.cleardb.net';
+$username = 'b607abb5e11746';
+$password = 'fe67bffa';
+$database = 'heroku_02f84be589b00af';
 
 try{
 	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
